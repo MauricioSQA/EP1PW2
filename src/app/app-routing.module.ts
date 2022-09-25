7afa31e19da7124ace048d78e89b050dbb88524e
Mauricio Quiñe <mfagotto@gmail.com>
2022-09-25 10:18:46 -0500

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { JuegosComponent } from './pages/juegos/juegos.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: InicioComponent},
+  {path:'juegos',component: JuegosComponent},
+  {path:'usuarios',component: UsuariosComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './common/main-header/main-header.component';
+import { MainFooterComponent } from './common/main-footer/main-footer.component';
+import { NosotrosComponent } from './home/nosotros/nosotros.component';
+import { NoticiasComponent } from './home/noticias/noticias.component';
+import { MainBannerComponent } from './home/main-banner/main-banner.component';
+import { MainNavComponent } from './common/main-nav/main-nav.component';
+import { JuegosComponent } from './pages/juegos/juegos.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainFooterComponent,
+    NosotrosComponent,
+    NoticiasComponent,
+    MainBannerComponent,
+    MainNavComponent,
+    JuegosComponent,
+    UsuariosComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
